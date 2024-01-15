461a88fdcc331296047f8a93ac33fe0327de1ab8
LAPTOP-JOJO4UK2\fede_ <fede_rock_85@hotmail.com>
2024-01-15 17:34:11 +0100

    validate user input

--- Chess_DomainModel/Chess_DomainModel/Player.cs
+++ Chess_DomainModel/Chess_DomainModel/Player.cs
@@ -1,4 +1,5 @@
 ﻿using Chess_DomainModel.Enums;
+using Chess_DomainModel.Pieces;
 
 namespace Chess_DomainModel
 {
@@ -13,22 +14,68 @@ namespace Chess_DomainModel
 
         public void Play(Board board)
         {
-            Console.WriteLine("Introduzca el origen");
-            var originInput = Console.ReadLine();
-            Console.WriteLine("Introduzca el destino");
-            var targetInput = Console.ReadLine();
+            var originInput = "";
+            var targetInput = "";
+            Piece piece;
+            Coordinate origin;
+            Coordinate target;
+            bool pieceCanMove;
 
-            var origin = new Coordinate(originInput);
-            var target = new Coordinate(targetInput);
+            
+            do
+            {
+                do
+                {
+                    do
+                    {
+                        Console.WriteLine("Introduzca un origen válido");
+                        originInput = Console.ReadLine();
 
-            var piece = board.GetPiece(origin);
+                    } while (ValidateInput(originInput));
 
-            var pieceCanMove = piece.IsValidMove(origin, target, board);
+                    do
+                    {
+                        Console.WriteLine("Introduzca un destino válido");
+                        targetInput = Console.ReadLine();
 
-            if(pieceCanMove) board.MovePiece(origin, target);
+                    } while (ValidateInput(targetInput));
+
+
+                    origin = new Coordinate(originInput);
+                    target = new Coordinate(targetInput);
+
+                    piece = board.GetPiece(origin);
+
+                    if (piece is NullPiece) Console.WriteLine("No hay una pieza de tu color en esa casilla");
+
+                } while (piece is NullPiece);
+                
+
+                pieceCanMove = piece.IsValidMove(origin, target, board);
+
+                if (!pieceCanMove) Console.WriteLine("No se puede realizar el movimiento");
+
+            } while (pieceCanMove);
+
+            board.MovePiece(origin, target);
 
             board.Write();
 
+        }
+
+        private bool ValidateInput(string input)
+        {
+            if(input == null) return false;
+            if(input.Length > 2) return false;
+
+            var firstChar = input[0].ToString().ToLower();
+            if (firstChar != "a" || firstChar != "b" || firstChar != "c" || firstChar != "d" || firstChar != "e" || firstChar !="f") return false;
+
+            var secondChar = input[1].ToString();
+            if(!int.TryParse(secondChar, out var result)) return false;
+            if (result <= 0 || result > 8) return false;
+
+            return true;
         }
     }
 }
