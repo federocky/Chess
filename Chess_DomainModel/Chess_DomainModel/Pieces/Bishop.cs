23a8d5e3a93f2b74571c64b0ae2d3141bb8bfab9
LAPTOP-JOJO4UK2\fede_ <fede_rock_85@hotmail.com>
2024-01-16 17:22:46 +0100

    program bishop movement

--- Chess_DomainModel/Chess_DomainModel/Pieces/Bishop.cs
+++ Chess_DomainModel/Chess_DomainModel/Pieces/Bishop.cs
@@ -15,7 +15,38 @@ namespace Chess_DomainModel.Pieces
 
         public override bool IsValidMove(Coordinate origin, Coordinate target, IBoard board)
         {
-            throw new NotImplementedException();
+            int rowDifference = Math.Abs(target.GetRow() - origin.GetRow());
+            int colDifference = Math.Abs(target.GetColumn() - origin.GetColumn());
+
+            if (rowDifference != colDifference) return false; // Movimiento no es diagonal            
+
+            var piecesInPath = GetDiagonalPathCoordinates(origin, target);
+            var arePieceInTheWay = board.ArePieceInPath(piecesInPath);
+
+            if (arePieceInTheWay) return false;
+
+            return true;
+        }
+
+
+        private List<Coordinate> GetDiagonalPathCoordinates(Coordinate start, Coordinate end)
+        {
+            List<Coordinate> pathCoordinates = new List<Coordinate>();
+
+            int rowDirection = Math.Sign(end.GetRow() - start.GetRow());
+            int colDirection = Math.Sign(end.GetColumn() - start.GetColumn());
+
+            int currentRow = start.GetRow() + rowDirection;
+            int currentCol = start.GetColumn() + colDirection;
+
+            while (currentRow != end.GetRow() || currentCol != end.GetColumn())
+            {
+                pathCoordinates.Add(new Coordinate(currentRow, currentCol));
+                currentRow += rowDirection;
+                currentCol += colDirection;
+            }
+
+            return pathCoordinates;
         }
     }
 }
