e019f82e1400d13911c81a64886c2a7e08d32f12
LAPTOP-JOJO4UK2\fede_ <fede_rock_85@hotmail.com>
2024-01-23 21:26:23 +0100

    Allow user to play more games after match is finished

--- Chess_DomainModel/Chess_DomainModel/Chess.cs
+++ Chess_DomainModel/Chess_DomainModel/Chess.cs
@@ -6,23 +6,45 @@ namespace Chess_DomainModel
     {
         private Turn turn { get; set; }
         private Board board { get; set; }
+        private bool keepPlaying { get; set; }
 
     public Chess()
         {
             turn = new Turn();
             board = new Board();
+            keepPlaying = false;
         }
 
         private void Play()
-        {
-            Console.WriteLine("Que comience el Ajedrez");
-            board.Write();
+        {            
 
             do
             {
-                turn.Play(board);
+                board = new Board();
+                Console.WriteLine("Que comience el Ajedrez");
+                board.Write();
+
+                do
+                {
+                    turn.Play(board);
                
-            } while (!IsGameFinished());
+                } while (!IsGameFinished());
+
+                
+                Console.WriteLine("CHESS MASTER");
+                var isNumber = false;
+
+                
+                Console.WriteLine("1- Juego Nuevo");
+                Console.WriteLine("*- Salir");
+                var input = Console.ReadLine();
+
+                isNumber = int.TryParse(input, out var option);
+
+                if (isNumber && option == 1) keepPlaying = true;  
+
+
+            } while (keepPlaying);
         }
 
         private bool IsGameFinished()
