d760e3b7547889f3e4c87be4bbb79b179e29295d
LAPTOP-FEDE\fede_ <fedeandresdeveloper@gmail.com>
2024-02-23 14:34:42 +0100

    knight move test

--- Chess_MVC_PassiveView/Chess_MVC_PassiveVies.UnitTests/Models/Pieces/KnightTests.cs
+++ Chess_MVC_PassiveView/Chess_MVC_PassiveVies.UnitTests/Models/Pieces/KnightTests.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Chess_MVC_PassiveView.Enums;
 using Chess_MVC_PassiveView.Models;
+using Chess_MVC_PassiveView.Models.Pieces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Chess_MVC_PassiveVies.UnitTests.Models.Pieces
 {
@@ -7,25 +9,41 @@ namespace Chess_MVC_PassiveVies.UnitTests.Models.Pieces
     public class KnightTests
     {
         private Board board;
+        private Knight knight;
 
         [TestInitialize]
         public void TestInitialize()
         {
             board = new Board();
+            knight = new Knight(PieceColor.White);
         }
 
         [TestMethod]
-        public void IsValidMove_ValidMove_True()
+        public void IsValidMove_ValidMoveFoward_True()
         {
             //Arrange
             var origin = new Coordinate("b1");
             var target = new Coordinate("a3");
 
             //Act
-            var result = board.IsValidMove(origin, target);
+            var result = knight.IsValidMove(origin, target, board);
 
             //Assert
             Assert.IsTrue(result);            
+        }
+
+        [TestMethod]
+        public void IsValidMove_ValidMoveBackward_True()
+        {
+            //Arrange
+            var origin = new Coordinate("a4");
+            var target = new Coordinate("b2");
+
+            //Act
+            var result = knight.IsValidMove(origin, target, board);
+
+            //Assert
+            Assert.IsTrue(result);
         }
 
         [TestMethod]
@@ -36,7 +54,7 @@ namespace Chess_MVC_PassiveVies.UnitTests.Models.Pieces
             var target = new Coordinate("b3");
 
             //Act
-            var result = board.IsValidMove(origin, target);
+            var result = knight.IsValidMove(origin, target, board);
 
             //Assert
             Assert.IsFalse(result);
@@ -50,7 +68,7 @@ namespace Chess_MVC_PassiveVies.UnitTests.Models.Pieces
             var target = new Coordinate("d3");
 
             //Act
-            var result = board.IsValidMove(origin, target);
+            var result = knight.IsValidMove(origin, target, board);
 
             //Assert
             Assert.IsFalse(result);
